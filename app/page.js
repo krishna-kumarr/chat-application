36ab32aa2dd272f808e44954a1d5b896493e4989
krishna-kumarr <kumarkrishna11231@gmail.com>
2025-08-28 17:08:07 +0530

    Checking auto deployment

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Signin from "@/modules/Signin";
 
 export default function Home() {
-
+  console.log("auto deployment working")
   return (
     <div>
-      <Signin  />
+      <Signin />
     </div>
   );
 }
- 
